a7b3539f02a6d15fc1329278ba7701c39f7c6870
Ben C. Forsberg <benf@bnj.com>
2014-07-30 16:02:01 -0700

    Added display formatting for phone.

--- controllers.js
+++ controllers.js
@@ -4,7 +4,22 @@ var Signifier = angular.module('Signifier', [])
 	return function( input ) {
 		input = input || '';
 		return input.replace(/[^0-9]+/g,'');
-	}
+	};
+})
+.filter('displayTel', function() {
+	return function(input) {
+		input = input || '';
+		input = input.replace(/[^0-9]+/g, '');
+		var out = input.slice(-4);
+		input = input.slice(0, -4);
+		while (input.length) {
+			var part = input.slice(-3);
+			input = input.slice(0, -3);
+			out = part + " " + out;
+		}
+		return out;
+	};
+
 })
 .controller('FormControl', function($scope) {
 });
